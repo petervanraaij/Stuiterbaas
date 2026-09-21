ffb6a558d3c6a81f852e2ae01e54101a661937eb
petervanraaij <p.vanraaij@outlook.com>
2026-09-21 22:10:08 +0200

    Connect reservation form to production worker

--- booking-config.js
+++ booking-config.js
@@ -1,5 +1,4 @@
 window.STUITERBAAS_BOOKING = Object.freeze({
-  endpoint: "",
-  turnstileSiteKey: ""
+  endpoint: "https://stuiterbaas-reserveren.stuiterbaas-456655a6.workers.dev",
+  turnstileSiteKey: "0x4AAAAAAE7_MvjrgPghze8U"
 });
-
